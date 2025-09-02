fbdb75cdd827c9447a659076997db30fd47a5501
hyeyun Hwang <hhyeyoun@naver.com>
2025-09-02 12:54:19 +0900

    0902과제

---
+++ unittest/basic_func.py
@@ -0,0 +1,19 @@
+#홀수짝수 판별하기
+
+def check_num(num):
+    return num % 2 == 0
+
+#평균구하기
+def check_avg(nums):
+    avg = sum(nums) / len(nums)
+    print(avg)
+
+
+#최댓값구하기
+def max_num(number):
+    return max(number)
+
+
+#최솟값구하기
+def min_num(number):
+    return min(number)
--- unittest/test_funcs.py
+++ unittest/test_funcs.py
@@ -1,6 +1,39 @@
 # TODO: 사용자 모듈 import
 
+from basic_func import check_num,check_avg,max_num,min_num
 
-# TODO: 아래의 코드를 삭제하고 unittest를 작성하세요.
-def test_always():
-    assert False, "Remove me!"
+
+
+
+def test_check_num():
+    assert check_num(2) == True
+    assert check_num(3) == False
+    assert check_num(0) == True
+    assert check_num(-1) == False
+
+
+def test_max_num():
+    assert max_num([1, 2, 3]) == 3
+    assert max_num([100, 99, 98]) == 100
+    assert max_num([-5, -2, -10]) == -2
+
+
+def test_min_num():
+    assert min_num([1, 2, 3]) == 1
+    assert min_num([100, 99, 98]) == 98
+    assert min_num([-5, -2, -10]) == -10
+
+
+def test_check_avg(capfd):  
+    check_avg([1, 2, 3, 4])   
+    out, err = capfd.readouterr()
+    assert out.strip() == "2.5"
+
+    check_avg([10, 20, 30])
+    out, err = capfd.readouterr()
+    assert out.strip() == "20.0"
+
+
+
+
+
